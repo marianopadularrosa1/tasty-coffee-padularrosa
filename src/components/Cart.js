9a4d548ca69510c7d2d1bdd01263098df1c075ec
Mariano <mariano.padularrosa@gmail.com>
2021-11-27 13:33:09 -0300

    Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -67,7 +67,8 @@ const Cart = () => {
   if(resultado){
     return(
     <div className="App">
-    <h1>Su codigo de Operacion es: {resultado.id}</h1>
+    <h1><Badge colorScheme="red">MUCHAS GRACIAS POR SU COMPRA</Badge></h1>
+    <h1>Su código de Operacion es: <Badge colorScheme="red">{resultado.id}</Badge></h1>
   </div>
     )
   }
